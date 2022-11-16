4bfe90024cd1756e5cfbc0b05a832beb35f9adb0
Steven Srun <steven.srun@outlook.com>
2022-11-15 19:38:23 -0800

    Include glew as the loader for imgui

--- AntonOpenGLTutorial/dependencies/imgui/imgui_impl_opengl3.cpp
+++ AntonOpenGLTutorial/dependencies/imgui/imgui_impl_opengl3.cpp
@@ -143,6 +143,8 @@
 #include "imgui_impl_opengl3_loader.h"
 #endif
 
+#include <GL/glew.h>
+
 // Vertex arrays are not supported on ES2/WebGL1 unless Emscripten which uses an extension
 #ifndef IMGUI_IMPL_OPENGL_ES2
 #define IMGUI_IMPL_OPENGL_USE_VERTEX_ARRAY
