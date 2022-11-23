e0a79c2fae8f093c291ccc5c5f28a4d15bd03f25
Steven Srun <steven.srun@outlook.com>
2022-11-23 09:02:03 -0800

    Remove test ImGui rendering

--- AntonOpenGLTutorial/source/renderer/Renderer.cpp
+++ AntonOpenGLTutorial/source/renderer/Renderer.cpp
@@ -204,6 +204,7 @@ void Renderer::DrawImGui()
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
+
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
